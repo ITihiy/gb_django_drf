91629db5ea0074aa9a7babd275be805bdc486f4c
Alexander Serdobincev <alexanderser@yandex.ru>
2022-03-22 15:15:31 +0300

    task_04 complete without starred task

--- gb_django_drf/settings.py
+++ gb_django_drf/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'corsheaders',
+    'django_filters',
 
     'drf_users',
     'todo_app',
@@ -139,3 +140,12 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+REST_FRAMEWORK = {
+    'DEFAULT_FILTER_BACKENDS': [
+        'django_filters.rest_framework.DjangoFilterBackend'
+    ],
+    'DEFAULT_PAGINATION_CLASS':
+        'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 100,
+}
--- todo_app/models.py
+++ todo_app/models.py
@@ -3,16 +3,19 @@ from django.db import models
 from drf_users.models import DRFUser
 
 
+class Project(models.Model):
+    name = models.CharField(max_length=256)
+    repo = models.URLField()
+    users = models.ManyToManyField(DRFUser)
+
+
 class TODOItem(models.Model):
     todo_text = models.TextField()
     is_active = models.BooleanField(default=True)
     created_at = models.DateTimeField(auto_created=True)
     updated_at = models.DateTimeField(auto_now=True)
     author = models.ForeignKey(DRFUser, on_delete=models.CASCADE)
+    project = models.ForeignKey(Project, on_delete=models.CASCADE)
 
-
-class Project(models.Model):
-    name = models.CharField(max_length=256)
-    repo = models.URLField()
-    users = models.ManyToManyField(DRFUser)
-    todo_items = models.ForeignKey(TODOItem, on_delete=models.CASCADE)
+    def delete(self, using=None, keep_parents=False):
+        self.is_active = False
