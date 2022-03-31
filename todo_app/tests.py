28fff9845dae4c3e75a4ffba5363edd307583d26
Alexander Serdobincev <alexanderser@yandex.ru>
2022-03-31 16:19:33 +0300

    task_08 complete

--- todo_app/tests.py
+++ todo_app/tests.py
@@ -1,3 +1,14 @@
-from django.test import TestCase
+from mixer.backend.django import mixer
+from rest_framework.test import APITestCase
 
-# Create your tests here.
+from drf_users.models import DRFUser
+from todo_app.models import Project
+
+
+class TestProject(APITestCase):
+    def test_creating_project(self):
+        DRFUser.objects.create_superuser('admin', 'admin@mail.local', 'Qwerty123456')
+        project = mixer.blend(Project)
+        self.client.login(username='admin@mail.local', password='Qwerty123456')
+        result = self.client.get('/api/drf_projects/1/')
+        self.assertEqual(result.data['name'], project.name)
